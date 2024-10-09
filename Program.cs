acc9882214f319b72a17b55a8ad504f298149651
Mr. Luyk <133881094+MrLuyk@users.noreply.github.com>
2024-10-09 17:53:24 +0000

    Wrote over the previous cod with the required "default code". Completed

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("My name is Collin Luyk");
+
+        //Using Variables
+        int age = 18;
+        string greeting = "Hello World";
+        string firstname = "Collin";
+        string lastname = "Luyk";
+        double money = 2.79;
+
+        Console.WriteLine(greeting + "! " + "My name is " + firstname + " "
+        + lastname + ". I am " + age + " years old. I just spent $" + money + " on an Energy Drink.");
+    }
+}
